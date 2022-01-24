4bc66bfdd9467a7153ce04e8817afa97f3c9d09f
uid164647 <uid164647@h2web235.infomaniak.ch>
2022-01-24 13:19:36 +0100

    clean code

--- web/app/themes/loggia/functions.php
+++ web/app/themes/loggia/functions.php
@@ -3,8 +3,6 @@
 
 function montheme_supports()
 {
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
     add_theme_support('menus');
     register_nav_menu('header', 'En tête du menu');
 }
@@ -18,7 +16,6 @@ add_action( 'wp_enqueue_scripts', 'custom_theme_assets', 100 );
 
 function montheme_register_assets()
 {
-    
 
     wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css', []);
     wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js', []);
@@ -60,9 +57,6 @@ function montheme_register_assets()
 
     wp_enqueue_script('jsslider');
     wp_enqueue_script('jsslidermin');
-
-
-
 
 }
 
